459413815ddd08e56ad93e78b34a9a008e2bef52
Marcin Rakowski <enamodeka@gmail.com>
2024-01-28 22:02:46 +0100

    add simple memory calculation

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
+const fs = require('fs');
+
 export {}; // silence TS errors re. class imports
 
-const fs = require('fs');
+
+const {formatBytes} = require('./helpers')
+
 const Simple = require('./algos/simple');
 const Trie = require('./algos/trie');
 const WordMap = require('./algos/wordmap');
@@ -22,8 +26,19 @@ console.timeEnd('Sorting');
 console.log('\n');
 
 
-new Simple(input, dictionaryListSorted);
+const mem: Record<string, any> = {};
+mem.initial = process.memoryUsage().heapUsed;
+const a1 = new Simple(input, dictionaryListSorted);
+mem.simple = process.memoryUsage().heapUsed - mem.initial;
 (new Trie()).longestWordInString(dictionaryListUnsorted, input);
+mem.trie = process.memoryUsage().heapUsed - mem.simple - mem.initial;
 (new WordMap(dictionaryListUnsorted)).search(input);
+mem.wordMap = process.memoryUsage().heapUsed - mem.trie - mem.simple - mem.initial;
 (new WordMapWithFrequency(dictionaryListUnsorted).search(input));
+mem.wordMapFreq = process.memoryUsage().heapUsed - mem.wordMap - mem.wordMap - mem.simple - mem.initial;
 
+for (let m in mem) {
+    const v = mem[m];
+    console.log(m, formatBytes(v));
+}
+console.log('Total memory: ', formatBytes(process.memoryUsage().heapUsed));
